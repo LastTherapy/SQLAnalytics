71288b274cc0a77ae1995942ef07832f6ef10897
helllomind <helllomind@hellomindmac.local>
2024-12-10 14:12:07 +0300

    get funcs

---
+++ sql/getfuncs.sql
@@ -0,0 +1,11 @@
+SELECT
+    n.nspname AS schema_name,
+    p.proname AS function_name,
+    pg_catalog.pg_get_function_result(p.oid) AS return_type,
+    pg_catalog.pg_get_function_arguments(p.oid) AS arguments,
+    pg_catalog.pg_get_functiondef(p.oid) AS function_definition
+FROM pg_proc p
+JOIN pg_namespace n ON p.pronamespace = n.oid
+WHERE n.nspname NOT IN ('pg_catalog', 'information_schema')
+  AND n.nspname !~ '^pg_toast'
+ORDER BY schema_name, function_name;
